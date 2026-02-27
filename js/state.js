09de05951101f951df7b592be289250b63bfe2b9
saikyo-rpg <naoki189jpp@gmail.com>
2026-02-27 21:53:56 +0900

    Update state.js

--- js/state.js
+++ js/state.js
@@ -4,9 +4,16 @@ export function createPlayer(){
     maxHp:120, hp:120,
     def:2,
 
+    dead:false,
+
+    // レベル/経験値
     lv:1,
     exp:0,
 
+    // お金
+    gold: 0,
+
+    // 攻撃（レベルで上がる）
     baseAtkMin:8,
     baseAtkMax:14,
     atkMin:8,
@@ -14,13 +21,15 @@ export function createPlayer(){
   };
 }
 
-export function makeEnemy(x,y,name,hp,atkMin,atkMax,def,expReward){
+export function makeEnemy(x,y,name,hp,atkMin,atkMax,def, expReward, goldReward){
   return {
     x,y,r:12,name,
     maxHp:hp, hp,
     atkMin, atkMax,
     def,
     expReward,
+    goldReward,
+
     alive:true,
     spawnX:x, spawnY:y,
     respawnTimer:0
@@ -29,8 +38,8 @@ export function makeEnemy(x,y,name,hp,atkMin,atkMax,def,expReward){
 
 export function createEnemies(){
   return [
-    makeEnemy(520,160,"スライム",80,6,11,1,12),
-    makeEnemy(560,320,"ゴブリン",110,8,14,2,20),
+    makeEnemy(520,160,"スライム",80,6,11,1, 12, 5),
+    makeEnemy(560,320,"ゴブリン",110,8,14,2, 20, 9),
   ];
 }
 
